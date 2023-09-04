4a0ad7746c370cada8e56f247ffc0c47bfb3626f
Edajvee <dechols@fubo.tv>
2023-09-04 18:29:45 -0500

    entry point refactor

--- src/main/java/org/boxingTournament/Main.java
+++ src/main/java/org/boxingTournament/Main.java
@@ -3,42 +3,44 @@ package org.boxingTournament;
 import org.boxingTournament.conference.Conference;
 import org.boxingTournament.fighter.Fighter;
 import org.boxingTournament.judges.Judge;
-import org.boxingTournament.match.Match;
 import org.boxingTournament.match.StatisticsAndOutcomes;
 import org.boxingTournament.tournament.Tournament;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //outcomes orchestrator
-        StatisticsAndOutcomes outcomes = new StatisticsAndOutcomes();
-
         //west fighters
-        List<Fighter> westFighters = new ArrayList<>();
-        Fighter westFighterOne = new Fighter("D78oyi 8o7y", 158, "5'9");
-        Fighter westFighterTwo = new Fighter("Hihe Echooihils", 158, "5'9");
-        westFighters.add(westFighterOne);
-        westFighters.add(westFighterTwo);
+        List<Fighter> westFighters = new ArrayList<>(
+                List.of(
+                        new Fighter("Mark Zuckerburg", 158, "5'9"),
+                        new Fighter("Kenny Wilson", 158, "5'9")
+                )
+        );
 
         //east fighters
-        List<Fighter> eastFighters = new ArrayList<>();
-        Fighter eastFighterOne = new Fighter("oijpoij", 158, "5'9");
-        Fighter eastFighterTwo = new Fighter("Hpojojoojs", 158, "5'9");
-        eastFighters.add(eastFighterOne);
-        eastFighters.add(eastFighterTwo);
+        List<Fighter> eastFighters = new ArrayList<>(
+                List.of(
+                        new Fighter("Drawson Standford", 158, "5'9"),
+                        new Fighter("Kerry Alis", 158, "5'9")
+                )
+        );
 
         //east and west conferences
         Conference westConference = new Conference(westFighters);
         Conference eastConference = new Conference(eastFighters);
 
         //judges
-        Set<Judge> judges = new HashSet<>();
-        judges.add(new Judge("Judge 1"));
-        judges.add(new Judge("Judge 2"));
-        judges.add(new Judge("Judge 3"));
+        List<Judge> judges = new ArrayList<>(
+                List.of(
+                        new Judge("Judge 1"),
+                        new Judge("Judge 2"),
+                        new Judge("Judge 3")
+                )
+        );
 
         //tournament structure
-        new Tournament(westConference, eastConference, judges).runTournament();
+        new Tournament(westConference, eastConference, judges, new StatisticsAndOutcomes()).runTournament();
     }
 }
