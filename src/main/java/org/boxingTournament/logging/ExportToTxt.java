5ecd367de63322f2be3ad9af364df51528e56f9c
Edajvee <dechols@fubo.tv>
2023-09-09 14:40:57 -0500

    add method to write to a text file

--- src/main/java/org/boxingTournament/logging/ExportToTxt.java
+++ src/main/java/org/boxingTournament/logging/ExportToTxt.java
@@ -1,5 +1,22 @@
 package org.boxingTournament.logging;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class ExportToTxt {
-    private final String TEXT_PATH = "src/main/java/org/boxingTournament/exports/Results.txt";
+    public static final String rootProjectPath = Paths.get(System.getProperty("user.dir")) + "/src/main/java/org/boxingTournament";
+
+    public static void log(String text) {
+        String textFilePath = rootProjectPath + "/exports/Results.txt";
+
+        try (FileOutputStream stream = new FileOutputStream(textFilePath, true)) {
+            byte[] bytes = text.getBytes();
+            stream.write(bytes);
+            stream.write("\n\n".getBytes());
+            System.out.println("Written to file");
+        } catch (IOException e) {
+            e.fillInStackTrace();
+        }
+    }
 }
